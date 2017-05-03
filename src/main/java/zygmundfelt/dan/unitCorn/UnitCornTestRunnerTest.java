8d2667004c20975a8f18452eb16d40f1dd28a605
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.local>
2017-05-03 19:36:33 -0400

    Fucking UnitCorn.

--- src/main/java/zygmundfelt/dan/unitCorn/UnitCornTestRunnerTest.java
+++ src/main/java/zygmundfelt/dan/unitCorn/UnitCornTestRunnerTest.java
@@ -4,17 +4,23 @@ import org.junit.*;
 public class UnitCornTestRunnerTest {
 
     UnitCornTestRunner unitCorn;
+    DummyTest dummyTest;
 
 
     @Before
     public void initialize() {
         unitCorn = new UnitCornTestRunner();
+        dummyTest = new DummyTest();
     }
 
     @Test
     public void runTestTest() {
         //Given
-
+        Result expected = new Result(true, "setANumberTest");
+        //When
+        Result actual = runTest(dummyTest, "setANumberTest");
+        //Then
+        Assert.assertTrue(expected.methodName.equals(actual.methodName) && expected.wasSuccess && actual.wasSuccess);
     }
 
 }
