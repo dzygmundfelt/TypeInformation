02b3db9f5898e2e8cba17d43da23fa7816a275ee
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.local>
2017-05-03 10:08:58 -0400

    Finished InstantiateClassHierarchy, though the test class needs a little bit of work.

---
+++ src/main/java/Part1/InstantiateClassHierarchy.java
@@ -0,0 +1,53 @@
+package Part1;
+import org.junit.*;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class InstantiateClassHierarchy {
+
+    static ArrayList<Object> instantiateClassHierarchy(Object o) {
+        Class cl = o.getClass();
+        ArrayList<Object> classInstanceArrayList = new ArrayList<Object>();
+
+        do {
+            try{
+                Object object = cl.newInstance();
+                classInstanceArrayList.add(object);
+                cl = cl.getSuperclass();
+            }
+            catch (Exception e) {
+                cl = cl.getSuperclass();
+            }
+        } while(cl != null);
+
+        for(Object object : classInstanceArrayList) {
+            int modifier = object.getClass().getModifiers();
+            if(modifier == 0 || modifier == 2) {
+                classInstanceArrayList.remove(object);
+            }
+        }
+
+        return classInstanceArrayList;
+    }
+
+    public static void main(String[] args) {
+        TreeMap map = new TreeMap();
+        ArrayList list = new ArrayList();
+
+        ArrayList<Object> forTreeMap = instantiateClassHierarchy(map);
+        ArrayList<Object> forList = instantiateClassHierarchy(list);
+
+        for(Object o : forTreeMap) {
+            System.out.println(o.getClass());
+        }
+
+        System.out.println();
+
+        for(Object o : forList) {
+            System.out.println(o.getClass());
+        }
+    }
+
+}
---
+++ src/main/java/Part1/InstantiateClassHierarchyTest.java
@@ -0,0 +1,74 @@
+package Part1;
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.TreeMap;
+
+/* Need to work out these tests. Instantiating objects and adding them to an
+   ArrayList won't lead to the .contains() method returning true, because, duh,
+   they're different instances of the same class.
+
+   This may not be too much trouble but I want to move on to something else.
+ */
+
+
+
+public class InstantiateClassHierarchyTest {
+
+    @Test
+    public void instantiateClassHierarchyTrueTest() {
+        //Given
+        TreeMap map = new TreeMap();
+        Object object = new Object();
+        ArrayList<Object> expected = new ArrayList<Object>();
+        expected.add(map);
+        expected.add(object);
+        //When
+        ArrayList<Object> actual = InstantiateClassHierarchy.instantiateClassHierarchy(map);
+        //Then
+        boolean result = true;
+
+        for(Object o : expected) {
+            if(!actual.contains(o)) {
+                result = false;
+            }
+        }
+
+        for(Object o : actual) {
+            if(!expected.contains(o)) {
+                result = false;
+            }
+        }
+
+        Assert.assertTrue(result);
+    }
+
+
+    @Test
+    public void instantiateClassHierarchyFalseTest() {
+        //Given
+        LinkedList list = new LinkedList();
+        ArrayList<Object> expected = new ArrayList<Object>();
+        expected.add(list);
+        //When
+        ArrayList<Object> actual = InstantiateClassHierarchy.instantiateClassHierarchy(list);
+        //Then
+        boolean result = true;
+
+        for(Object o : expected) {
+            if(!actual.contains(o)) {
+                result = false;
+            }
+        }
+
+        for(Object o : actual) {
+            if(!expected.contains(o)) {
+                result = false;
+            }
+        }
+
+        Assert.assertFalse(result);
+
+    }
+}
